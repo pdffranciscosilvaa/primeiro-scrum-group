207db0eb38fc8ac08a22e2c8596ee742a029ca38
Francisco Lisboa <francisco_ls_lima@estudante.sesisenai.org.br>
2025-02-14 09:54:49 -0300

    Criado o arquivo index.js e ajustadas as configurações

--- __test__/cubo.test.js
+++ __test__/cubo.test.js
@@ -3,7 +3,7 @@ import Cubo from "../model/Cubo.js";
 describe("Testes da classe Cubo", ()=> {
     test("Teste da área total do cubo", ()=>{
         const aresta = 4
-        areaTotalEsperada = 6 * (aresta * aresta)
+        const areaTotalEsperada = 6 * (aresta * aresta)
         const cubo = new Cubo(aresta)
 
         expect(cubo.calcularAreaTotal()).toBeCloseTo(areaTotalEsperada,2)
@@ -11,7 +11,7 @@ describe("Testes da classe Cubo", ()=> {
 
     test("Teste do volume do cubo", ()=>{
         const aresta = 4
-        volumeEsperado =  aresta * aresta * aresta
+       const volumeEsperado =  aresta * aresta * aresta
         const cubo = new Cubo(aresta)
         
         expect(cubo.calcularVolume()).toBeCloseTo(volumeEsperado,2)
